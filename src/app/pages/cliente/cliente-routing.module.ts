085eb4dc707311d8e8a3f2725a25aa45cc4362ca
brunocoronha <brunocoronha@gmail.com>
2024-08-29 15:27:00 -0300

    adsfasdf

--- src/app/pages/cliente/cliente-routing.module.ts
+++ src/app/pages/cliente/cliente-routing.module.ts
@@ -1,14 +1,20 @@
+import { ClienteFormModule } from './cliente-form/cliente-form.module';
+import { ClientePageModule } from './../../cliente/cliente.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ClientePage } from './cliente.component';
+import { ClientePageComponent } from './cliente.component';
 import { CadastroPage } from '../cadastro/cadastro.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ClientePage,
+    component: ClientePageComponent,
   }
+  {
+    path: '',
+    loadChildren: () => import('ClientePageModule').then( m => m.ClientePageModule)
+  },
 ];
 
 @NgModule({
